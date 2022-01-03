da9ade347578c9f19f0c6d738ddff13446ac70f6
tay2von <tayvonlewis0@gmail.com>
2022-01-02 22:12:33 -0600

    updated app to work after hard drive crash

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
       </MainContentStyled>
     </div>
-  );
+  )
 }
 
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Particles from 'react-particles-js';
 import styled from 'styled-components';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GithubIcon from '@material-ui/icons/GitHub';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 function HomePage() {
   return (
@@ -9,9 +12,24 @@ function HomePage() {
         <Particles />
       </div>
       <div className="typography">
-        <h1>Hi I'm <span>Lorem Ipsum</span></h1>
+        <h1>Welcome, I'm<span>Tayvon Lewis</span></h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium natus doloremque illo deserunt iste ullam facere magni sunt debitis quis!
+        </p>
+        <div className="icons">
+          <div className="i-facebook">
+            <FacebookIcon />
+          </div>
+          <div className="i-github">
+            <GithubIcon />
+          </div>
+
+          <div className="i-youtube">
+            <YouTubeIcon />
+          </div>
+        </div>
       </div>
-    </HomePageStyled>
+    </HomePageStyled >
   )
 }
 
@@ -19,5 +37,17 @@ const HomePageStyled = styled.header`
   width: 100%;
   height: 100vh;
   position: relative; 
+  .p-particles-js{
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .typography{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
 `;
 export default HomePage;
